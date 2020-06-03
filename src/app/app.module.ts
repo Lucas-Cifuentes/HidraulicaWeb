49951129ff0fc3642138dcb5a28012ac8d455e1f
lucas cifuentes <lucasdcifuentes@gmail.com>
2020-06-03 20:02:14 -0300

    first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselEmpresasComponent } from './components/carousel-empresas/carousel-empresas.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    FooterComponent,
+    CarouselEmpresasComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
