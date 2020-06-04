62accbd1b3247fa613b1392106468609f7d62d1b
lucas cifuentes <lucasdcifuentes@gmail.com>
2020-06-04 13:47:31 -0300

    nuevo producto component

--- src/app/components/nuevo-producto/nuevo-producto.component.ts
+++ src/app/components/nuevo-producto/nuevo-producto.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-nuevo-producto',
   templateUrl: './nuevo-producto.component.html',
-  styleUrls: ['./nuevo-producto.component.scss']
+  styleUrls: ['./nuevo-producto.component.scss'],
 })
 export class NuevoProductoComponent implements OnInit {
+  nuevoProducto = {
+    Codigo: '',
+    Descripcion: '',
+    Tipo: '',
+  };
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  codigoProducto(codigo) {
+    this.nuevoProducto.Codigo = codigo.toUpperCase();
   }
 
+  descripcionCodigo(descripcion) {
+    this.nuevoProducto.Descripcion = descripcion.toUpperCase();
+  }
+
+  tipoProducto(tipo) {
+    this.nuevoProducto.Tipo = tipo.toUpperCase();
+  }
+
+  agregar() {
+    this.db
+      .collection('Productos')
+      .add(this.nuevoProducto)
+      .then(() => {
+        console.log('Producto Agregado');
+      });
+  }
 }
