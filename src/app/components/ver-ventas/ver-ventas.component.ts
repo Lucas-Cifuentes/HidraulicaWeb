c0fe9fa17218a974ed965084d1e78c83c4175f58
lucas cifuentes <lucasdcifuentes@gmail.com>
2020-06-05 11:44:31 -0300

    vista de ventas

--- src/app/components/ver-ventas/ver-ventas.component.ts
+++ src/app/components/ver-ventas/ver-ventas.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ver-ventas',
   templateUrl: './ver-ventas.component.html',
-  styleUrls: ['./ver-ventas.component.scss']
+  styleUrls: ['./ver-ventas.component.scss'],
 })
 export class VerVentasComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  f = new Date();
 
+  fecha = `${this.f.getDate()} / ${
+    this.f.getMonth() + 1
+  } / ${this.f.getFullYear()}`;
 }
